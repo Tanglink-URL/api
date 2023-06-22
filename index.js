12a2581f95f36368e982133d7d0e76f861d2570a
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-22 20:04:13 -0300

    create test endpoint

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const app = require('express')()
 
 const PORT = process.env.PORT || 8080
 
+
+
+app.get('/ping', (req, res) =>{
+
+    const apiKey = req.query.apiKey
+
+    res.send({data: 'pong'})
+})
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Live on port ${PORT}`))
 
 
