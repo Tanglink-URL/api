3d3b5f5a57831255d501fdd3184fb8d7fa86f26a
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-25 02:12:53 -0300

    create mongoDB connect function

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/ping', (req, res) =>{
 
 
 app.listen(PORT, () => console.log(`Live on port ${PORT}`))
-
+connectMongo()
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,21 +33,30 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }catch(e){
-    console.log(e)
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connectMongo(){
+    try{
+        await mongoose.connect(uri)
+        console.log("Connected to MongoDB")
+    }catch(e){
+        console.error(e)
+    }
 }
-run().catch(console.dir);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   }catch(e){
+//     console.log(e)
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
