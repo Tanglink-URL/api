78010a74c482b56453802e6511f3d2030f1dc988
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-25 01:57:22 -0300

    mongoDB setup

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://" + process.env.MONGOUSERNAME + ":" + process.env.MONGOPASSWORD + "@tanglink.pqrecr6.mongodb.net/?retryWrites=true&w=majority";
+
 const app = require('express')()
 
 const PORT = process.env.PORT || 8080
@@ -13,9 +19,34 @@ app.get('/ping', (req, res) =>{
 
 
 
-
-
-
 app.listen(PORT, () => console.log(`Live on port ${PORT}`))
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }catch(e){
+    console.log(e)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
